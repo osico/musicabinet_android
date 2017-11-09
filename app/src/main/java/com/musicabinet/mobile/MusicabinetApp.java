e9b4a8f3ef4df6592e652f4183ed9397e4d6a78e
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-11-09 15:20:42 +0200

    Enable crashlytics. Change ic_launcher icon.

---
+++ app/src/main/java/com/musicabinet/mobile/MusicabinetApp.java
@@ -0,0 +1,17 @@
+package com.musicabinet.mobile;
+
+import android.app.Application;
+
+/**
+ * @author Kirchhoff-
+ */
+
+public class MusicabinetApp extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
--- app/src/main/java/com/musicabinet/mobile/SplashActivity.kt
+++ app/src/main/java/com/musicabinet/mobile/SplashActivity.kt
@@ -2,11 +2,15 @@ package com.musicabinet.mobile
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Fabric.with(this, Crashlytics())
     }
 }
