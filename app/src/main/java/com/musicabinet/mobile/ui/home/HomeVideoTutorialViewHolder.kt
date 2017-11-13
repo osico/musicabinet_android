a175c5a47a1a3d0fdb36b629cb6ee382e2e310a7
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-11-13 16:38:43 +0200

    Added layout for home_video_tutorial.

---
+++ app/src/main/java/com/musicabinet/mobile/ui/home/HomeVideoTutorialViewHolder.kt
@@ -0,0 +1,19 @@
+package com.musicabinet.mobile.ui.home
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.musicabinet.mobile.R
+import com.musicabinet.mobile.model.home.HomeDataElement
+import kotlinx.android.synthetic.main.item_home_video_tutorial.view.*
+
+/**
+ * @author Kirchhoff-
+ */
+class HomeVideoTutorialViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(element: HomeDataElement) {
+        with(element) {
+            itemView.ivTutorial.setImageResource(R.drawable.ic_musicabinet)
+        }
+    }
+}
