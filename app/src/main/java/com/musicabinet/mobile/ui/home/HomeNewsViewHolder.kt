22a631051182bb90c58bda4e0e4c3483cfb4d8b0
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-11-13 14:26:29 +0200

    Added base layout and ViewHolder for HomeNews element.

---
+++ app/src/main/java/com/musicabinet/mobile/ui/home/HomeNewsViewHolder.kt
@@ -0,0 +1,20 @@
+package com.musicabinet.mobile.ui.home
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.musicabinet.mobile.model.home.HomeDataElement
+import kotlinx.android.synthetic.main.item_home_news.view.*
+
+/**
+ * @author Kirchhoff-
+ */
+class HomeNewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(element: HomeDataElement) {
+        with(element) {
+            itemView.tvTitle.text = name
+            itemView.tvText.text = dataField.text
+            itemView.tvDate.text = date
+        }
+    }
+}
