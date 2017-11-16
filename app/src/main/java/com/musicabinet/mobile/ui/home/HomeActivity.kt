c8e5c5c8fa262765e970b1498cc66880a7a8d1e6
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-11-16 15:56:18 +0200

    Obtain click on education and home in slide menu.

--- app/src/main/java/com/musicabinet/mobile/ui/home/HomeActivity.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/home/HomeActivity.kt
@@ -1,20 +1,29 @@
 package com.musicabinet.mobile.ui.home
 
+import android.content.Context
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
 import com.musicabinet.mobile.R
+import com.musicabinet.mobile.ui.slidemenu.SlideMenuActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
 /**
  * @author Kirchhoff-
  */
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : SlideMenuActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
         viewPager.adapter = HomeAdapter(this)
         tabLayout.setupWithViewPager(viewPager)
     }
+
+    override fun inflateLayout(): View {
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        return inflater.inflate(R.layout.activity_home, null, false)
+    }
+
 }
--- app/src/main/java/com/musicabinet/mobile/ui/slidemenu/SlideMenuActivity.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/slidemenu/SlideMenuActivity.kt
@@ -1,23 +1,52 @@
 package com.musicabinet.mobile.ui.slidemenu
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.musicabinet.mobile.R
+import com.musicabinet.mobile.ui.home.HomeActivity
+import com.musicabinet.mobile.ui.instrument.InstrumentActivity
 import kotlinx.android.synthetic.main.activity_slide_menu.*
+import kotlinx.android.synthetic.main.view_slide_menu.*
 
 /**
  * @author Kirchhoff-
  */
-abstract class SlideMenuActivity : AppCompatActivity() {
+abstract class SlideMenuActivity : AppCompatActivity(), SlideMenuContract.View {
+
+    private lateinit var presenter: SlideMenuContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slide_menu)
 
         contentLayout.addView(inflateLayout())
+
+        presenter = SlideMenuPresenter(this)
+
+        educationLayout.setOnClickListener { presenter.onEducationClick() }
+        homeLayout.setOnClickListener { presenter.onHomeClick() }
     }
 
-
     protected abstract fun inflateLayout(): View
+
+    override fun moveToEducation() {
+        val intent = Intent(this, InstrumentActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun moveToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun logout() {
+    }
+
+    override fun moveToMyAccount() {
+    }
+
 }
