57b81c96c4b5051308f021ead81f81b96db56d6e
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-11-16 12:08:25 +0200

    Implements base layout for activity_slide_menu, and create main concept on inflating child activity.

--- app/src/main/java/com/musicabinet/mobile/ui/slidemenu/SlideMenuActivity.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/slidemenu/SlideMenuActivity.kt
@@ -2,15 +2,22 @@ package com.musicabinet.mobile.ui.slidemenu
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.musicabinet.mobile.R
+import kotlinx.android.synthetic.main.activity_slide_menu.*
 
 /**
  * @author Kirchhoff-
  */
-class SlideMenuActivity : AppCompatActivity() {
+abstract class SlideMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slide_menu)
+
+        contentLayout.addView(inflateLayout())
     }
+
+
+    protected abstract fun inflateLayout(): View
 }
