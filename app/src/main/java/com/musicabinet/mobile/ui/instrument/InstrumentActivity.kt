5a112365c29bce065807ee12149bda916b278c64
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-11-16 13:55:41 +0200

    Make InstrumentActivity inherited from SlideMenuActivity.

--- app/src/main/java/com/musicabinet/mobile/ui/instrument/InstrumentActivity.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/instrument/InstrumentActivity.kt
@@ -1,31 +1,38 @@
 package com.musicabinet.mobile.ui.instrument
 
+import android.content.Context
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
 import com.musicabinet.mobile.Injection
 import com.musicabinet.mobile.R
 import com.musicabinet.mobile.extensions.setVisible
 import com.musicabinet.mobile.model.instrument.InstrumentDataElement
 import com.musicabinet.mobile.ui.instrument.adapter.InstrumentAdapter
+import com.musicabinet.mobile.ui.slidemenu.SlideMenuActivity
 import kotlinx.android.synthetic.main.activity_instrument.*
 
 /**
  * @author Kirchhoff-
  */
-class InstrumentActivity : AppCompatActivity(), InstrumentContract.View {
+class InstrumentActivity : SlideMenuActivity(), InstrumentContract.View {
 
     private lateinit var instrumentAdapter: InstrumentAdapter
     private lateinit var presenter: InstrumentContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_instrument)
 
         presenter = InstrumentPresenter(Injection.provideRepository(), this)
 
         presenter.loadInstrumentList()
     }
 
+    override fun inflateLayout(): View {
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        return inflater.inflate(R.layout.activity_instrument, null, false)
+    }
 
     override fun showLoading(visible: Boolean) {
         progressBar.setVisible(visible)
