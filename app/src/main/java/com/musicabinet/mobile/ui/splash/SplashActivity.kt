11613613e558a4d95e6c4f3223162c079cbd2677
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-11-21 20:25:03 +0200

    Move SplashActivity to new package.

--- app/src/main/java/com/musicabinet/mobile/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/splash/SplashActivity.kt
@@ -1,8 +1,9 @@
-package com.musicabinet.mobile
+package com.musicabinet.mobile.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.musicabinet.mobile.R
 import com.musicabinet.mobile.ui.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
