6939299e7f4a6484d1d50700fd1765b48e0b4b01
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-12-15 16:38:39 +0200

    Added spinner to SoundView.

--- app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/view/SoundView.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/view/SoundView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.ArrayAdapter
 import com.musicabinet.mobile.R
 import kotlinx.android.synthetic.main.view_sound.view.*
 
@@ -30,5 +31,12 @@ class SoundView : ConstraintLayout {
         cDrums.setOnClickListener { cDrums.isChecked = !cDrums.isChecked }
         cBass.setOnClickListener { cBass.isChecked = !cBass.isChecked }
         cKeys.setOnClickListener { cKeys.isChecked = !cKeys.isChecked }
+
+        val list = ArrayList<String>()
+        list.add("First")
+        list.add("Second")
+        list.add("Third")
+        val adapter = ArrayAdapter<String>(context, R.layout.item_sound_spinner, list)
+        sRoad.adapter = adapter
     }
 }
