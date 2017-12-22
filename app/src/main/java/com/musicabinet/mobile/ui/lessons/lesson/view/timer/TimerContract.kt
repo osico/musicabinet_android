8737adbd88767fb607d6192b6977748a28de6e17
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-12-22 11:36:03 +0200

    Added TimerContract.

---
+++ app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/view/timer/TimerContract.kt
@@ -0,0 +1,15 @@
+package com.musicabinet.mobile.ui.lessons.lesson.view.timer
+
+/**
+ * @author Kirchhoff-
+ */
+interface TimerContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/view/timer/TimerPresenter.kt
@@ -0,0 +1,8 @@
+package com.musicabinet.mobile.ui.lessons.lesson.view.timer
+
+/**
+ * @author Kirchhoff-
+ */
+class TimerPresenter(private val view: TimerContract.View) : TimerContract.Presenter {
+
+}
