7873311997421f7afb492cc62dae9187721b184d
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-12-27 10:49:54 +0200

    Change visibility of SoundView according to accompaniments size.

--- app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/view/SoundView.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/view/SoundView.kt
@@ -5,6 +5,7 @@ import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import com.musicabinet.mobile.R
+import com.musicabinet.mobile.extensions.setVisible
 import com.musicabinet.mobile.model.lesson.remote.Accompaniment
 import kotlinx.android.synthetic.main.view_sound.view.*
 
@@ -35,6 +36,8 @@ class SoundView : ConstraintLayout {
 
 
     fun setAccompaniments(accompaniments: Set<Accompaniment>) {
+
+        setVisible(!accompaniments.isEmpty())
 
         val list = ArrayList<Accompaniment>(accompaniments)
         val adapter = SoundViewAdapter(context, R.layout.item_sound_spinner, list)
