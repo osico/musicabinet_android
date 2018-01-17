1c71783c603e5a9a20e058be31f3bbed1e0a520d
Kirchhoff- <kirchhoff.19.92@gmail.com>
2018-01-17 14:21:08 +0200

    Change home news request id, and fixs item_home_news layout.

--- app/src/main/java/com/musicabinet/mobile/repository/DefaultMusicabinetRepository.kt
+++ app/src/main/java/com/musicabinet/mobile/repository/DefaultMusicabinetRepository.kt
@@ -12,7 +12,7 @@ import com.musicabinet.mobile.model.register.UserInfo
 object DefaultMusicabinetRepository : MusicabinetRepository {
 
     private const val HOME_VIDEO_ID = "1eb9efe2-1428-476a-a69c-46c1cebb9dad"
-    private const val HOME_NEWS_ID = "1eb9efe2-1428-476a-a69c-46c1cebb8dab"
+    private const val HOME_NEWS_ID = "1eb9efe2-1428-476a-a69c-46c1cebb9dab"
     private const val HOME_TUTORIALS_ID = "1eb9efe2-1428-476a-a69c-46c1cebb9daa"
     private const val REQUEST_ITEM_COUNT = 5
 
