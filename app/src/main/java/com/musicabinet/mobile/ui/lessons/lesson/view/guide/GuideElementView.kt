67b7948fec18f963a8cd1a799e1b7a54f53a3875
Kirchhoff- <kirchhoff.19.92@gmail.com>
2018-01-19 09:51:31 +0200

    Added base layout for GuideElementView.

---
+++ app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/view/guide/GuideElementView.kt
@@ -0,0 +1,29 @@
+package com.musicabinet.mobile.ui.lessons.lesson.view.guide
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.musicabinet.mobile.R
+
+/**
+ * @author Kirchhoff-
+ */
+class GuideElementView : ConstraintLayout {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    fun init() {
+        LayoutInflater.from(context).inflate(R.layout.view_guide_element, this, true)
+    }
+}
