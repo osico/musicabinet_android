e6c8c5ab760a065188be83c407cacfc5aeba0769
Kirchhoff- <kirchhoff.19.92@gmail.com>
2018-01-26 16:30:57 +0200

    Obtain click on element of NoteImageAdapter.

--- app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/note/adapter/image/NoteImageAdapter.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/note/adapter/image/NoteImageAdapter.kt
@@ -9,15 +9,28 @@ import com.musicabinet.mobile.utils.BaseRecyclerAdapter
 /**
  * @author Kirchhoff-
  */
-class NoteImageAdapter(items: List<NoteElement>)
-    : BaseRecyclerAdapter<NoteImageViewHolder, NoteElement>(items) {
+class NoteImageAdapter : BaseRecyclerAdapter<NoteImageViewHolder, NoteElement>,
+        BaseRecyclerAdapter.OnItemClickWithPositionListener<NoteElement> {
+
+    private var selectedList: MutableList<Boolean>
+
+    constructor(items: List<NoteElement>) : super(items) {
+        selectedList = MutableList(items.size, { false })
+        setOnItemClickWithPosition(this)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             NoteImageViewHolder(parent.inflate(R.layout.item_note_image))
 
     override fun onBindViewHolder(holder: NoteImageViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), selectedList[position])
     }
 
+    override fun onItemClick(item: NoteElement, position: Int) {
+        val result = !selectedList[position]
+        selectedList = MutableList(getDataSet().size, { false })
+        selectedList[position] = result
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/note/adapter/image/NoteImageViewHolder.kt
+++ app/src/main/java/com/musicabinet/mobile/ui/lessons/lesson/note/adapter/image/NoteImageViewHolder.kt
@@ -3,6 +3,7 @@ package com.musicabinet.mobile.ui.lessons.lesson.note.adapter.image
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.musicabinet.mobile.extensions.loadLessonImage
+import com.musicabinet.mobile.extensions.setVisible
 import com.musicabinet.mobile.model.lesson.machine.note.image.NoteElement
 import kotlinx.android.synthetic.main.item_note_image.view.*
 
@@ -11,9 +12,10 @@ import kotlinx.android.synthetic.main.item_note_image.view.*
  */
 class NoteImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(element: NoteElement) {
+    fun bind(element: NoteElement, isSelected: Boolean) {
         with(element) {
             itemView.ivNote.loadLessonImage(image.id)
+            itemView.selectedView.setVisible(isSelected)
         }
     }
 }
